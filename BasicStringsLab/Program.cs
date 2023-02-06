b096476e917f8a001a6b6d292202aebe093a8943
KARBIDE17 <stevewoods17@gmail.com>
2023-02-05 20:28:59 -0500

    First commit for Basic Strings Lab, second half incoming soon.

---
+++ BasicStringsLab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Metrics;
+do
+{ 
+Console.WriteLine("Please write me a sentence so that i can split it up into individual words");
+
+string fullSentence = Console.ReadLine();
+
+string[] splitResult = fullSentence.Split(' ');
+
+
+for (int i = 0; i < splitResult.Length; i++)
+{
+    Console.WriteLine(splitResult[i]);
+}
+Console.WriteLine("Would you like to learn about another student? y/n?"); //continue running with a prompt at the end to see if they want to stop. 
+string choice2 = Console.ReadLine();
+while (choice2 != "y" && choice2 != "Y")
+{
+    Console.WriteLine("Goodbye!");
+    return;
+
+}
+} while (true) ;
